a11187ebd0f1c70ffee7d92054801c0cb5bb2fc1
Ch N V S S Durga Prasad <chodagiriprasad5@gmail.com>
2025-05-16 22:29:41 +0530

    commit

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,38 +1,33 @@
-
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SidebarWrapper from "./Sidebar";
 import { useIsMobile } from "@/hooks/use-mobile";
-import { SidebarTrigger } from "@/components/ui/sidebar";
 import { useAuth } from "@/contexts/AuthContext";
 import BackgroundElements from "./BackgroundElements";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { isAuthenticated } = useAuth();
   const isMobile = useIsMobile();
-  
+
   return (
     <div className="flex flex-col min-h-screen">
       <BackgroundElements />
       <Navbar />
+
       {isAuthenticated && isMobile ? (
         <SidebarWrapper>
-          <div className="flex flex-col min-h-[calc(100vh-64px)]">
-            {isMobile && (
-              <div className="mb-4">
-                <SidebarTrigger className="block md:hidden" />
-              </div>
-            )}
+          <div className="flex-grow flex flex-col min-h-[calc(100vh-64px)]">
+            {/* Removed SidebarTrigger from here */}
             <div className="flex-grow p-4">{children}</div>
             <Footer />
           </div>
         </SidebarWrapper>
       ) : (
-        <div className="flex flex-col min-h-[calc(100vh-64px)]">
+        <>
           <main className="flex-grow">{children}</main>
           <Footer />
-        </div>
+        </>
       )}
     </div>
   );
